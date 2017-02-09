1c0767b369eb5d2cf84380cddb193a7cc6311ba1
Dmitry Orlov <me@mosquito.su>
2017-02-10 01:17:49 +0300

    [fix] add classifiers

--- setup.py
+++ setup.py
@@ -13,4 +13,22 @@ setup(
     install_requires=[
         "lsm-db",
     ],
+    platforms="all",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Internet',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
 )
