cb5bca8c7b812b79bff323becf8177d6200a28b9
Dmitry Orlov <me@mosquito.su>
2019-01-24 02:00:32 +0300

    [fix] bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lsm-db-extras',
-    version='0.3.0',
+    version='0.4.0',
     url="https://github.com/mosquito/lsm-db-extras",
     author='Dmitry Orlov',
     author_email='me@mosquito.su',
